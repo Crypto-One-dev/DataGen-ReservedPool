2f66875491625c98edc88326373f79d4315d3f6e
William <kan881271@gmail.com>
2021-08-11 12:12:15 -0400

    fix second release schedule

--- contracts/ReservedPool.sol
+++ contracts/ReservedPool.sol
@@ -4,9 +4,8 @@
  * B-Datagray Reserved Pool
  * Tokens inside this smart contract: 1,500,500.00 #DG
  * 100% locked until 21st December 2023
- * 750,000.00 #DGs will be released between 21st December 2023 and 15th December 2024, relasing them in 12 events, one every 30 days
- * Other 750,000.00 #DGs will be locked in the Reserved Pool for 2,555 days(7 years) after the token launch.
- * The Reserved Pool will then automatically unlock 250 #DGs for the next 3,000 days.
+ * 750,000.00 #DGs will be released between 21st December 2023 and 15th December 2024, releasing them in 12 events, one every 30 days.
+ * Other 750,000.00 #DGs will be locked in the Reserved Pool and from 15th December 2024, releasing them in 24 events, one every 30 days.
  */
 
 pragma solidity ^0.8.0;
@@ -20,28 +19,24 @@ contract ReservedPool is Ownable, ReentrancyGuard {
   using SafeMath for uint256;
   
   /* First release tokens */
-  uint256 public frAmount = 750000 * (10**20);
+  uint256 public frAmount = 750000 * (10**18);
   /* First release start time(21/12/2023 00:00:00) */
   uint256 public frStart = 1703116800;
   /* Second release tokens */
-  uint256 public srAmount = 750000 * (10**20);
+  uint256 public srAmount = 750000 * (10**18);
   /* Second release start time */
-  uint256 public srStart;
+  uint256 public srStart = 1734220800;
   /* the address of the token contract */
 	IERC20 public dataGen;
   /* the address of the company wallet */
   address public companyWallet;
-  /* Deployed time of DataGen */
-  uint256 public deployedTime;
 
   event SetCompanyWalletAddress(address indexed user, address indexed companyWallet); 
 
   /*  initialization, set the token address */
-  constructor(IERC20 _dataGen, address _companyWallet, uint256 _deployedTime) {
+  constructor(IERC20 _dataGen, address _companyWallet) {
     dataGen = _dataGen;
     companyWallet = _companyWallet;
-    deployedTime = _deployedTime;
-    srStart = deployedTime.add(2555 * 24 * 3600);
   }
 
   modifier firstRelease() {
@@ -50,12 +45,10 @@ contract ReservedPool is Ownable, ReentrancyGuard {
   }
 
   function releaseDataGen() public firstRelease nonReentrant {
-    // require(block.timestamp >= frStart, "Pool is Still locked.");
     require(dataGen.balanceOf(address(this)) > 0, "Zero #DG left.");
 
-    if(srStart > block.timestamp ) {
-      // uint256 epochs = block.timestamp.sub(frStart).div(30 * 24 * 3600).add(1);
-      uint256 epochs = (block.timestamp.sub(frStart)).div(300).add(1);
+    if(block.timestamp < srStart ) {
+      uint256 epochs = block.timestamp.sub(frStart).div(30 * 24 * 3600).add(1);
       if (epochs > 12) epochs = 12;
 
       uint256 releaseAmount = frAmount.div(12);
@@ -76,10 +69,10 @@ contract ReservedPool is Ownable, ReentrancyGuard {
         dataGen.transfer(companyWallet, (balance.sub(srAmount)));
       }
 
-      uint256 epochs = (block.timestamp.sub(srStart)).div(24 * 3600).add(1);
-      if (epochs > 3000) epochs = 3000;
+      uint256 epochs = (block.timestamp.sub(srStart)).div(30 * 24 * 3600).add(1);
+      if (epochs > 24) epochs = 24;
 
-      uint256 releaseAmount = srAmount.div(3000);
+      uint256 releaseAmount = srAmount.div(24);
       uint256 leftAmount = srAmount.sub(releaseAmount.mul(epochs));
 
       require(balance > leftAmount, "Already released.");
--- migrations/1_datagen_migration.js
+++ migrations/1_datagen_migration.js
@@ -7,11 +7,11 @@ const companyWallet = "0x2a9138bE66f7a2b1370C10c6B489A7a5354bB896";
 module.exports = async function(deployer) {
   try {
     await deployer.deploy(DataGen);
-    const transaction = await web3.eth.getTransaction(DataGen.transactionHash);
-    const deployedBlock = await web3.eth.getBlock(transaction.blockNumber);
-    const deployedTime = deployedBlock.timestamp;
+    // const transaction = await web3.eth.getTransaction(DataGen.transactionHash);
+    // const deployedBlock = await web3.eth.getBlock(transaction.blockNumber);
+    // const deployedTime = deployedBlock.timestamp;
 
-    await deployer.deploy(ReservedPool, DataGen.address, companyWallet, deployedTime);
+    await deployer.deploy(ReservedPool, DataGen.address, companyWallet);
   } catch (error) {
     console.log(error);
   }
